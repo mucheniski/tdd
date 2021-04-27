0717b4f46c2a6c6f499c95def6cc7e621e4a8083
mucheniski <mucheniski@gmail.com>
2021-04-27 19:51:58 -0300

    Refatorado para usar o repository mockado

--- src/test/java/com/example/tdd/service/BookingServiceTest.java
+++ src/test/java/com/example/tdd/service/BookingServiceTest.java
@@ -1,12 +1,21 @@
 package com.example.tdd.service;
 
+import com.example.tdd.model.BookingModel;
+import com.example.tdd.repository.BookginRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 public class BookingServiceTest {
@@ -24,6 +33,19 @@ public class BookingServiceTest {
 
     @Autowired
     BookgingService bookgingService;
+
+    @MockBean
+    BookginRepository bookginRepository;
+
+    @BeforeEach
+    public void setUp() {
+        LocalDate checkIn = LocalDate.parse("2021-11-01");
+        LocalDate checkOut = LocalDate.parse("2021-11-11");
+        BookingModel bookingModel = new BookingModel("1", "Bruna", checkIn, checkOut, 2);
+
+        Mockito.when(bookginRepository.findByReseveName(bookingModel.getReserveName())).thenReturn(Optional.of(bookingModel));
+
+    }
 
     @Test
     public void bookingServiceDaysCalculatorTest() {
